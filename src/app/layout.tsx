f8ab48cc659ac25ad2497713c93c66eaea6c8764
Evan Gerland--Molinier <evan.gerlandmolinier@mezcalito.fr>
2026-01-05 22:47:15 +0100

    Revert "Home page et page de Bienvenue"
    This reverts commit 266166d76b63b17c13b793ed564590fa642cb459.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="fr">
-            <body className='bg-(--background)'>
+            <body>
                 <main className="mx-auto min-h-screen w-full max-w-6xl p-4">{children}</main>
             </body>
         </html>
