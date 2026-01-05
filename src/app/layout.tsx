29abc538d8c9c34457175eb0e1ce911447ec38d4
Evan Gerland--Molinier <evan.gerlandmolinier@mezcalito.fr>
2026-01-05 15:01:39 +0100

    feat(config): prettier and eslint config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'SAE501 - Escape Game - Groupe 1',
-  description: "Description de l'escape game",
-}
+    title: 'SAE501 - Escape Game - Groupe 1',
+    description: "Description de l'escape game",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-      <body>
-        <main className="mx-auto min-h-screen w-full max-w-6xl p-4">
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="fr">
+            <body>
+                <main className="mx-auto min-h-screen w-full max-w-6xl p-4">{children}</main>
+            </body>
+        </html>
+    );
 }
