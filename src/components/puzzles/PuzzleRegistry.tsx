6d500a7934ee16255e6f7e3096e86864aa433e67
Evan Gerland--Molinier <evan.gerlandmolinier@mezcalito.fr>
2026-01-08 01:49:17 +0100

    fix(onSolve): callback logic placed outside modals

--- src/components/puzzles/PuzzleRegistry.tsx
+++ src/components/puzzles/PuzzleRegistry.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { BoltIcon, StarIcon, TrophyIcon, XCircleIcon } from '@heroicons/react/24/solid';
 
-import ChromaticPuzzle1 from '@/components/SCENARIO/1-ChromaticGame/ChromaticPuzzle';
+import { ChromaticPuzzle } from '@/components/puzzles/ChromaticPuzzle';
 import { CodingPuzzle } from '@/components/puzzles/CodingPuzzle';
 import { OrientationPuzzle } from '@/components/puzzles/OrientationPuzzle';
 import { SpinPuzzle } from '@/components/puzzles/SpinPuzzle';
@@ -177,7 +177,7 @@ export const DefeatScreen = () => (
 
 export const PUZZLE_COMPONENTS = {
     'test-puzzle': TestPuzzle,
-    'chromatic-puzzle': ChromaticPuzzle1,
+    'chromatic-puzzle': ChromaticPuzzle,
     'orientation-puzzle': OrientationPuzzle,
     'spin-puzzle': SpinPuzzle,
     'coding-puzzle': CodingPuzzle,
