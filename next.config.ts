4c1999f925bc3fce97d8971f313163207de7b297
Evan Gerland--Molinier <evan.gerlandmolinier@mezcalito.fr>
2026-01-09 08:46:13 +0100

    feat(front base): added FRONT_BASE_URI to .env

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
+const FRONT_BASE_URI = process.env.NEXT_PUBLIC_FRONT_BASE_URI || '';
+
 const nextConfig: NextConfig = {
+    basePath: FRONT_BASE_URI,
+    assetPrefix: FRONT_BASE_URI,
     output: 'export',
     trailingSlash: true,
     reactStrictMode: true,
