627eb4e67f7c89e2f9a31e00e3b07ef5b3c812a9
bondonri <Ismael.Bondon-Robert@etu.univ-grenoble-alpes>
2026-01-13 17:22:03 +0100

    (fix)[geolocation] fix bug iOS gps et amélioration du fonctionnement

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -17,9 +17,13 @@ export interface GeolocationData {
     distance: number | null;
 }
 
-export function useGeolocation(targetLat = 45.2031, targetLong = 5.702213) {
-    const { data: orientation } = useOrientation();
-
+export function useGeolocation(
+    targetLat = 45.2031,
+    targetLong = 5.702213,
+    externalOrientation?: OrientationData
+) {
+    const internalOrientation = useOrientation();
+    const orientation = externalOrientation || internalOrientation.data;
     const [data, setData] = useState<GeolocationData>({
         latitude: null,
         longitude: null,
@@ -37,11 +41,11 @@ export function useGeolocation(targetLat = 45.2031, targetLong = 5.702213) {
 
     // Sélection du bon heading
     const heading = useMemo(() => {
-        if (data.speed && data.speed > 1 && data.gpsHeading !== null) {
+        if (data.gpsHeading !== null && data.speed !== null && data.speed > 2) {
             return data.gpsHeading;
         }
-        return orientation.heading ?? null;
-    }, [data.speed, data.gpsHeading, orientation.heading]);
+        return orientation.heading;
+    }, [data.gpsHeading, data.speed, orientation.heading]);
 
     // Compass
     const compass = useMemo(() => {
@@ -50,7 +54,7 @@ export function useGeolocation(targetLat = 45.2031, targetLong = 5.702213) {
             data.longitude === null ||
             heading === null ||
             data.accuracy === null ||
-            data.accuracy > 50
+            data.accuracy > 100
         ) {
             return null;
         }
@@ -63,7 +67,7 @@ export function useGeolocation(targetLat = 45.2031, targetLong = 5.702213) {
             relativeAngle,
             arrow: angleToDirection8(relativeAngle),
         };
-    }, [data, heading, targetLat, targetLong]);
+    }, [data.latitude, data.longitude, data.accuracy, heading, targetLat, targetLong]);
 
     const onSuccess = useCallback(
         (position: GeolocationPosition) => {
