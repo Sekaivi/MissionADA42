5893de6b7574a58269560796772852f2288de0b4
Evan Gerland--Molinier <evan.gerlandmolinier@mezcalito.fr>
2026-01-15 16:44:56 +0100

    feat(PWA): add installable PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
+
 import type { Metadata } from 'next';
 
 import { Providers } from '@/app/providers';
+import { ServiceWorkerRegister } from '@/app/sw-register';
 
 import './globals.css';
 
 export const metadata: Metadata = {
     title: 'SAE501 - Escape Game - Groupe 1',
     description: "Description de l'escape game",
+    manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -17,6 +21,7 @@ export default function RootLayout({
     return (
         <html lang="fr" suppressHydrationWarning>
             <body>
+                <ServiceWorkerRegister />
                 <Providers>{children}</Providers>
             </body>
         </html>
